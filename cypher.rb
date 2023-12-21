37eab1225dc0661a3b3816f3dd4883bcefe3b201
Stefano <snebo54@hotmail.com>
2023-12-21 15:13:10 +0100

    Cleaned up code

--- cypher.rb
+++ cypher.rb
@@ -5,10 +5,7 @@ $LETTERS = ("a".."z").each.with_index(1).to_h
 $CC_LOCK = 3
 
 def n_loop(chr)
-  num = chr.downcase.ord - 96 + $CC_LOCK
-  # if z => 26 + 3
-
-  if num > 26
+  if (num = chr.downcase.ord - 96 + $CC_LOCK) > 26
     num -= $CC_LOCK
     $CC_LOCK.times  do
       num += 1
@@ -30,27 +27,4 @@ message.split("").each do |chr|
   else code.push(chr)
   end
 end
-
 puts code.join('')
-
-# $CC_LOCK= 5
-# $LETTERS = ("a".."z").each.with_index(1).to_h
-
-# puts ("Enter you secret message ->  ")
-# message = gets.chomp
-
-# #convert each letter with the CC_lock
-# def cc_convert (word)
-#   new_w = []
-#   code = []
-#   word.split("").each do |chr|
-#     if chr.match?(/\A[a-z]\Z/)
-#       new_w.push(chr == chr.upcase ? ($LETTERS.key($LETTERS[chr.downcase]+$CC_LOCK)).upcase 
-#       : $LETTERS.key($LETTERS[chr]+ $CC_LOCK))
-#     else new_w.push(chr)
-#     end
-#   end
-#   return new_w.join('')
-# end
-
-# puts cc_convert(message)
