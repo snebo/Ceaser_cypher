ffd06d0f0682508af080463406d4458163ca0b65
Stefano <snebo54@hotmail.com>
2023-12-21 12:22:44 +0100

    Reconstruction code to all for the looping in number efficiently

--- cypher.rb
+++ cypher.rb
@@ -1,25 +1,24 @@
 # Simple encryption for conversations
-$CC_LOCK= 5
-$LETTERS = ("a".."z").each.with_index(1).to_h
+# using the ord method
 
-puts ("Enter you secret message ->  ")
-message = gets.chomp
+# $CC_LOCK= 5
+# $LETTERS = ("a".."z").each.with_index(1).to_h
 
-#convert each letter with the CC_lock
-def cc_convert (word)
-  new_w = []
-  code = []
-  word.split("").each do |chr|
-    if chr.match?(/\A[a-z]\z/)
-      new_w.push(chr == chr.upcase ? ($LETTERS.key($LETTERS[chr.downcase]+$CC_LOCK)).upcase 
-      : $LETTERS.key($LETTERS[chr]+ $CC_LOCK))
-    else new_w.push(chr)
-    end
-  end
-  return new_w.join('')
-end
+# puts ("Enter you secret message ->  ")
+# message = gets.chomp
 
-puts cc_convert(message)
-# if the char + cc_lock is greater than 26, minus 26 from it and print that letter
+# #convert each letter with the CC_lock
+# def cc_convert (word)
+#   new_w = []
+#   code = []
+#   word.split("").each do |chr|
+#     if chr.match?(/\A[a-z]\z/)
+#       new_w.push(chr == chr.upcase ? ($LETTERS.key($LETTERS[chr.downcase]+$CC_LOCK)).upcase 
+#       : $LETTERS.key($LETTERS[chr]+ $CC_LOCK))
+#     else new_w.push(chr)
+#     end
+#   end
+#   return new_w.join('')
+# end
 
-# this works, but runs into an issue when you move past the hash value
+# puts cc_convert(message)
