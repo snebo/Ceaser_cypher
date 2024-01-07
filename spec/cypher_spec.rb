2dbb1475a0ee7ffedef48418336f6f95225f5877
Stefano <snebo54@hotmail.com>
2024-01-07 22:11:05 +0100

    add decrypt test

--- spec/cypher_spec.rb
+++ spec/cypher_spec.rb
@@ -1,16 +1,24 @@
 require './lib/cypher'
 
-describe Cypher do
-  cy = CeaserCypher.new(3)
+describe CeaserCypher do
   describe '#encrypt' do
+    cy = CeaserCypher.new(3)
+
     it 'return the encrypted message correctly' do
-      expect(cy.encrypt('Hello my friend')).to equal('Khoor ob iulhqg')
+      expect(cy.encrypt('Hello my friend')) == ('Khoor pb iulhqg')
     end
     it 'encrypt the message leaving symbols and number intact' do
-      expect(cy.encrypt("1HF2''nn")).to equal("1KI2''qq")
+      expect(cy.encrypt("1HF2''nn")) == ("1KI2''qq")
     end
     it 'return empty if message is empty' do
-      expect(cy.encrypt('')).to equal('')
+      expect(cy.encrypt('')) == ('')
+    end
+  end
+
+  describe '# decrypt' do
+    cy = CeaserCypher.new(3)
+    it 'Decodes the given message' do
+      expect(cy.decrypt("Mx'w svomrk fixxiv xler ibtigxih")) == "It's working better than expected"
     end
   end
 end
